35127096525b658cbacbff8a5a3b5ccc8440eb78
Michael W <welchm27@gmail.com>
2023-05-09 18:17:04 -0400

    Default fileReturn() method in TaxPayer

--- Lab09.1/src/gov/irs/TaxPayer.java
+++ Lab09.1/src/gov/irs/TaxPayer.java
@@ -21,4 +21,7 @@ public interface TaxPayer {
     public static final double SALARIED_TAX_RATE = 0.30;
     
     public void payTaxes();
+    default public void fileReturn() {          // have to mark it as default in order to add code
+        System.out.println("Return filed by US Mail");
+    }
 }
