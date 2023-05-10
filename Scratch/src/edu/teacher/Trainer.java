93433554392057969e655b7ad51b642b8d9027c5
Michael W <welchm27@gmail.com>
2023-05-10 10:14:25 -0400

    Scratch teacher optional HW

---
+++ Scratch/src/edu/teacher/Trainer.java
@@ -0,0 +1,47 @@
+package edu.teacher;
+
+class Trainer {
+    // static methods, shared by all
+
+    // instance variables or fields
+    private String name;
+    private String course;
+
+    // constructors
+    public Trainer() {
+
+    }
+
+    public Trainer(String name, String course) {
+        setName(name);
+        setCourse(course);
+    }
+
+    // business methods
+    public void teach() {
+        System.out.println(" trains a room full of SDE students");
+    }
+
+    // accessor methods
+    // get/set name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    // toString method
+    public String toString() {
+        return getClass().getSimpleName() + ": name=" + getName() + ", course=" + getCourse();
+    }
+}
